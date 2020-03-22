7971d768b46f69bdd1e72a5c6b1cb39494e8055e
Sandro Braun <supermario94123@gmail.com>
2020-03-22 17:19:06 +0100

    updates requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python_pascal_voc",
+    version="0.2",
+    description="Python package to work with Pascal VOC20xx, including Pascal Parts. Inspired by mprat/pascal-voc-python and twuilliam/pascal-part-py",
+    author="",
+    author_email="",
+    license="BSD",
+    url="",
+    packages=find_packages(),
+    scripts=[],
+    zip_safe=False,
+)
